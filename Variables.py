1769b236903c6f71056e1b7d261ed3e7a12c7c7c
MelonMunchr <166346249+MelonMunchr@users.noreply.github.com>
2024-08-04 05:57:04 -0400

    Create Variables.py
    code

---
+++ Variables.py
@@ -0,0 +1,24 @@
+x = "joini"
+
+print(x)
+
+y = 5 
+
+print(y)
+
+
+#x and y are variable 
+
+#we gave x the value of joini, and we print the Vairable in the concale
+
+#thus we get joini as output
+
+#the same thang applies to y but it prints 5 insted of joini
+
+#and we can chage the variables name,
+
+#we mostly name Variables to what they do 
+
+#variables  ontainers that hold values. They can be used to store anything 
+
+#like data types like integers and strings 
